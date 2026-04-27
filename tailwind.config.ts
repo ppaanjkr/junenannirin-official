edc83fb3c32625dd4f71dc46b4f01f15a667e1ee
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-04-27 16:21:25 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,35 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        kanit: ["var(--font-kanit)"],
+      },
+      colors: {
+        pinkAccent: "#ffe4ef",
+        pinkSecondary: "#ff6fa3",
+        textMain: "#2D3135",
+        textSub: "#71767A"
+      },
+      boxShadow: {
+        soft: "0 10px 30px rgba(0,0,0,0.08)",
+        sm: "0 2px 8px rgba(255,143,182,.18)"
+      },
+      textColor: {
+        accent: "#ffe4ef",
+        secondary: "#ff6fa3",
+        main: "#2D3135",
+        sub: "#71767A"
+      },
+      keyframes: {
+        pulseSoft: {
+          '50%': {
+            boxShadow: '0 0 0 8px rgba(255,111,163,0)',
+          },
+        },
+      },
+      animation: {
+        pulseSoft: 'pulseSoft 1.6s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
